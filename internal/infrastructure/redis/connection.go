eee50d6ded78ea660b270b714ead0932871e5997
otie173 <otie173.email@gmail.com>
2025-10-27 18:31:18 +0300

    add: настроил соединение с кэшем в виде редиса

--- internal/infrastructure/redis/connection.go
+++ internal/infrastructure/redis/connection.go
@@ -1,1 +1,26 @@
 package redis
+
+import (
+	"log"
+	"strings"
+
+	"github.com/otie173/skyland-auth/internal/config"
+	"github.com/redis/go-redis/v9"
+)
+
+func NewClient(cfg *config.Config) *redis.Client {
+	var sb strings.Builder
+	sb.WriteString(cfg.RedisHost)
+	sb.WriteString(":")
+	sb.WriteString(cfg.RedisPort)
+
+	addr := sb.String()
+	log.Println(addr)
+
+	return redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: "",
+		DB:       0,
+	},
+	)
+}
