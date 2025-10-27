9a75fa974c23d2bc99b649a1661682cc45ac5145
otie173 <otie173.email@gmail.com>
2025-10-27 18:32:09 +0300

    add: добавил соединение с базой данных в виде postgres

--- internal/infrastructure/postgresql/connection.go
+++ internal/infrastructure/postgresql/connection.go
@@ -1,1 +1,32 @@
 package postgresql
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"github.com/otie173/skyland-auth/internal/config"
+)
+
+func NewConnection(cfg *config.Config) (*sqlx.DB, error) {
+	connStr := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		cfg.DBHost, cfg.DBPort, cfg.DBUser, cfg.DBPassword, cfg.DBName,
+	)
+
+	db, err := sqlx.Connect("postgres", connStr)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(25)
+	db.SetConnMaxLifetime(5 * time.Minute)
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
